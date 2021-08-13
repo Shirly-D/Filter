a9b050246740e363f76135775c8b088c85b2f633
Shirly <shirlyd@axioned.com>
2021-08-13 19:08:12 +0530

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,5 @@
-var elem = document.querySelectorAll('.filter-elem');
-for (var i = 0; i < elem.length; i++) {
-    elem[i].addEventListener('click', function(e){
-        if (this == e.target) {
-       var gallery = e.target.getAttribute("data-gallery");
-    }
-  });
-}
-
-function garden() {
-    
-}
+var all = document.querySelector('.all');
+var 
 
 
 
