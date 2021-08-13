879cb939b384ebb169fbfa83852f50eac4f20a73
Shirly <shirlyd@axioned.com>
2021-08-13 18:59:43 +0530

    initial js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-/* Author: 
+var elem = document.querySelectorAll('.filter-elem');
+for (var i = 0; i < elem.length; i++) {
+    elem[i].addEventListener('click', function(e){
+        if (this == e.target) {
+       var gallery = e.target.getAttribute("data-gallery");
+    }
+  });
+}
 
-*/
-
-
-
-
-
+function garden() {
+    
+}
 
 
 
