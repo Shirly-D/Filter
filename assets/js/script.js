f7a2ec5bc825cfaf87947572f8d47ab2e63bc995
Shirly <shirlyd@axioned.com>
2021-08-14 00:05:16 +0530

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 var all = document.querySelector('.all');
-var 
+var lawn = document.querySelector('.lawn-care');
+var garden = document.querySelector('garden-care');
+var planting = document.querySelector('planting');
 
+function abc() {
+all.addEventListener('click', function(e) {
+    if (all.innerHTML == 'all') {
+        
+    }
+});
+
+}
+abc();
 
 
 
