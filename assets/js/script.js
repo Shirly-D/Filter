45267e27ce0a64f856c75e2b049b4670f9ba0aa1
Shirly <shirlyd@axioned.com>
2021-08-14 02:38:57 +0530

    js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,33 @@ var filterElem = document.querySelectorAll('.filter');
 for (var i = 0; i < filterElem.length; i++){
     filterElem[i].addEventListener('click', function(e){
         if (this == e.target) {
-            var elem = e.target.getAttribute('data-gallery');
+            var elem = e.target.innerHTML;
+            for (var k = 0; k < filterElem.length; k++){
+                filterElem[i].classList.remove("");
+            }
+            filterElem[i].classList.add("active");
             console.log(e.target);
-            filter();
+            console.log(elem);
+            filter(elem);
         }
     })
 }
 
-function filter() {
+function filter(elemFilter) {
     var element = document.querySelectorAll('.filter-image');
+    
     for (var j = 0; j < element.length; j++) {
-        if ('data-gallery' == element,innerHTML) {
-            element[j].className += "active";
+        var a = element[j].getAttribute("data-gallery");
+        element[j].classList.remove('active');
+        element[j].classList.remove('none');
+        if (elemFilter == a || elemFilter == "all") {
+            element[j].className += " active";
         } else {
-            element[j].className += "none";
+            element[j].className += " none";
         }
+        console.log(elemFilter);
     }
 }
-
 
 
 
